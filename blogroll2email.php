fc1ec651fcd692bfda3f9fd7f1dc13189450cf35
Peter Molnar <hello@petermolnar.eu>
2015-07-15 13:12:22 +0100

    Create blogroll2email.php

---
+++ blogroll2email.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: blogroll2email
+Plugin URI: https://github.com/petermolnar/blogroll2email
+Description: Pulls RSS, Atom and microformats URLs from blogroll links and sends them as email
+Version: 0.1
+Author: Peter Molnar <hello@petermolnar.eu>
+Author URI: http://petermolnar.eu/
+License: GPLv3
+*/
+
+/*  Copyright 2010-2014 Peter Molnar ( hello@petermolnar.eu )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 3, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
